831b7789f75eed92e62de6c7ec49984cf8591a3b
Saidi Sodiq <saidisodiq96@gmail.com>
2023-09-30 01:45:41 +0100

    initial commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const Video = mongoose.model('Video', videoSchema);
 //setting up multer for video file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb (null, 'uploads/');
+        cb (null, './uploads');
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
