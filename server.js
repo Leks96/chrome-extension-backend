823e3c346a0bd8c37237b3c372d987995903a048
Saidi Sodiq <saidisodiq96@gmail.com>
2023-09-30 03:31:15 +0100

    fixed database issues

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 const express = require ('express');
 const mongoose = require ('mongoose');
 const multer = require ('multer');
-const path = require ('path');
-//const fs = require ('fs');
+const cloudinary = require ('cloudinary').v2
 
 const app = express();
 port = process.env.PORT || 3050;
+          
+cloudinary.config({ 
+  cloud_name: 'dglpy94yq', 
+  api_key: '349563784769639', 
+  api_secret: 'digX16WEKTBHwdeC4X0BNflG7gQ' 
+});
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -24,7 +29,6 @@ db.on('error', (error) => {
 });
 
 //the video schema
-
 const videoSchema = new mongoose.Schema({
     title: String,
     filename: String,
@@ -33,34 +37,42 @@ const videoSchema = new mongoose.Schema({
 const Video = mongoose.model('Video', videoSchema);
 
 //setting up multer for video file uploads
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb (null, './uploads');
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb (null, uniqueSuffix + path.extname(file.originalname));
-    },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ 
     storage: storage,
-    limits: {
-        fileSize: 1000 * 1024 * 1024
-    },
-});
+}); 
 
 //handle video uploads
 app.post('/api/upload', upload.single('video'), async (req, res) => {
     try {
         //create a new video doc in mongoDB
         const { title } = req.body;
-        const { filename } = req.file;
+        const videoFileBuffer = req.file.buffer;
 
-        const video = new Video({ title, filename });
-        await video.save();
+        //upload video to cloudinary
+        const uploadResponse = await cloudinary.uploader.upload_stream(
+            { resource_type: 'video' },
+            async (error, result) => {
+                if (error) {
+                    console.error(error);
+                    res.status(500).json({ message: 'Error uploading video' });
+                } else {
+                    //save vido metadata to the mongoDB
+                    const { public_id, secure_url } = result;
 
-        res.status(201).json({ message: 'video uploaded successfully' });
+                    //create new video doc in mongoDB
+                    const video = new Video({
+                        title,
+                        filename: public_id //store public_id
+                    });
+                    await video.save(); //save metadata in mongoDB
+
+                    res.status(201).json({ message: 'Video uploaded succesfully' } )
+                }
+            }
+        );
+
+        videoFileBuffer.pipe(uploadResponse)
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'server error' })
